072936180279a8cbc86026605353e5c4e6ef0cd6
Nguyen Vu Minh Quang <163733286+Wan172005@users.noreply.github.com>
2026-05-08 21:55:45 +0700

    Update SecurityConfig to permit all requests
    Commented out the previous security filter chain configuration and updated the request authorization to permit all requests.

--- customer/src/main/java/com/yas/customer/config/SecurityConfig.java
+++ customer/src/main/java/com/yas/customer/config/SecurityConfig.java
@@ -16,21 +16,29 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    // @Bean
+    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    //     return http
+    //         .authorizeHttpRequests(auth -> auth
+    //             .requestMatchers("/actuator/prometheus", "/actuator/health/**",
+    //                 "/swagger-ui", "/swagger-ui/**", "/error", "/v3/api-docs/**").permitAll()
+    //             .requestMatchers("/storefront/customer/**").hasRole("CUSTOMER")
+    //             .requestMatchers("/storefront/**").permitAll()
+    //             .requestMatchers("/backoffice/**").hasRole("ADMIN")
+    //             .anyRequest().authenticated())
+    //         .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+    //         .build();
+    // }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         return http
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/actuator/prometheus", "/actuator/health/**",
-                    "/swagger-ui", "/swagger-ui/**", "/error", "/v3/api-docs/**").permitAll()
-                .requestMatchers("/storefront/customer/**").hasRole("CUSTOMER")
-                .requestMatchers("/storefront/**").permitAll()
-                .requestMatchers("/backoffice/**").hasRole("ADMIN")
-                .anyRequest().authenticated())
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .anyRequest().permitAll())
             .build();
     }
-
+    
     @Bean
     public JwtAuthenticationConverter jwtAuthenticationConverterForKeycloak() {
         Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter = jwt -> {
