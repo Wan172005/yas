f94ffdb04008933161a51ee8d63bd39014cc8913
Nguyen Vu Minh Quang <163733286+Wan172005@users.noreply.github.com>
2026-05-12 10:07:59 +0700

    Modify SecurityFilterChain to permit all requests

--- location/src/main/java/com/yas/location/config/SecurityConfig.java
+++ location/src/main/java/com/yas/location/config/SecurityConfig.java
@@ -17,16 +17,25 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    // @Bean
+    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    //     return http
+    //         .authorizeHttpRequests(auth -> auth
+    //             .requestMatchers("/actuator/prometheus", "/actuator/health/**",
+    //                 "/swagger-ui", "/swagger-ui/**", "/error", "/v3/api-docs/**").permitAll()
+    //             .requestMatchers("/storefront/**").permitAll()
+    //             .requestMatchers("/backoffice/**").hasRole("ADMIN")
+    //             .anyRequest().authenticated())
+    //         .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+    //         .build();
+    // }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/actuator/prometheus", "/actuator/health/**",
-                    "/swagger-ui", "/swagger-ui/**", "/error", "/v3/api-docs/**").permitAll()
-                .requestMatchers("/storefront/**").permitAll()
-                .requestMatchers("/backoffice/**").hasRole("ADMIN")
-                .anyRequest().authenticated())
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .anyRequest().permitAll())
             .build();
     }
 
