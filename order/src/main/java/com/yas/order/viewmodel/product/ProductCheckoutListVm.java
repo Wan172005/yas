0bf1c9c798f0eded5125c22f440088e66c47dcf1
Nguyen Vu Minh Quang <163733286+Wan172005@users.noreply.github.com>
2026-05-21 14:48:03 +0700

    Change fields to private in ProductCheckoutListVm
    Updated field modifiers to private for encapsulation.

--- order/src/main/java/com/yas/order/viewmodel/product/ProductCheckoutListVm.java
+++ order/src/main/java/com/yas/order/viewmodel/product/ProductCheckoutListVm.java
@@ -1,13 +1,18 @@
 package com.yas.order.viewmodel.product;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder(toBuilder = true)
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductCheckoutListVm {
-    Long id;
-    String name;
-    Double price;
-    Long taxClassId;
+
+    private Long id;
+    private String name;
+    private Double price;
+    private Long taxClassId;
 }
