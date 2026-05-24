75eedcdcfe5e231097737d727b5e10ac72fcb5a0
Nguyen Vu Minh Quang <163733286+Wan172005@users.noreply.github.com>
2026-05-24 16:52:01 +0700

    Add comment for Jenkins commit test
    Added a comment for Jenkins commit test.

--- product/src/main/java/com/yas/product/service/ProductDetailService.java
+++ product/src/main/java/com/yas/product/service/ProductDetailService.java
@@ -23,6 +23,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+//Commit test  jenkins
+
 /**
  * Service class responsible for managing product recommendations.
  */
